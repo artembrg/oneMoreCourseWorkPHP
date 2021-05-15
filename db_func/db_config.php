fe74ca93f1795a5fc581fee1521d1b20b13f3679
artembrg@gmail.com <artembrg@gmail.com>
2021-05-15 13:34:54 +0300

    db_conf update

--- db_func/db_config.php
+++ db_func/db_config.php
@@ -2,9 +2,9 @@
 function conf()
 {
     return array(
-        'host' => 'localhost',
-        'dbname' => 'kr',
-        'user' => 'root',
-        'password' => ''
+        'host' => 'eu-cdbr-west-01.cleardb.com',
+        'dbname' => 'heroku_1b096164c8ace0f',
+        'user' => 'b5004f5264a148',
+        'password' => '5f4b7ec0'
     );
 }
